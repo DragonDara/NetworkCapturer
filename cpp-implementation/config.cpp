2354b920be9b41e1883d6672afaddd40c4823cf4
root <root@mail.local>
2019-08-19 18:04:52 +0600

    config db

---
+++ cpp-implementation/config.cpp
@@ -0,0 +1,4 @@
+#include </home/dara/sniffer/headers/config.hpp>
+const string address = "tcp://localhost:3306";
+const string user = "root";
+const string pass = "P@$$w0rd";
---
+++ headers/config.hpp
@@ -0,0 +1,13 @@
+#include <string>
+using std::string;
+// #define ADDRESS tcp://192.168.101.16:3306
+// #define USER root
+// #define PASS P@$$w0rd
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+extern const string address;
+extern const string user;
+extern const string pass;
+
+#endif
